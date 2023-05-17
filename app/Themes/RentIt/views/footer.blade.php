06777447bb28a86831e0eda2a133f029998de311
tawhid12 <tawhid99@gmail.com>
2023-05-17 20:23:34 +0600

    design correction

--- app/Themes/RentIt/views/footer.blade.php
+++ app/Themes/RentIt/views/footer.blade.php
@@ -22,28 +22,9 @@
             <div class="row">
 
                 <div class="col-sm-12">
-                    {{--<p class="btn-row text-center">
+                    <p class="btn-row text-center">
 
-						<?php
-						if(get_theme_mod( 'footer_enable_social_buttons', true )){
-						$all_arr = get_theme_mod( 'footer_social_buttons' );
-						$new_arr = [];
-						if ( $all_arr['url'] ?? false ) {
-						foreach ( $all_arr['url'] as $k => $v ) {
-						?>
-                        <a target="_blank" href="{{ $all_arr['url'][$k] ?? '' }}"
-                           class="btn btn-theme btn-icon-left {{ $all_arr['type'][$k] ?? '' }}">
-                            <i class="fa {{ $all_arr['icon'][$k] ?? '' }}"></i>{{ $all_arr['text'][$k] ?? '' }}
-                        </a>
-
-						<?php
-
-						}
-						}
-						}
-						?>
-
-                    </p>--}}
+						
                     <div class="copyright">
                         {{get_theme_mod('footer_copyright', '©'.  date('Y', time()) . '  Rent It — An One Page Rental Car Theme made with passion by jThemes Studio')}}
                     </div>

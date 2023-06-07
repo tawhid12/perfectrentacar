ef438236e0bd6680fe6bbed9d473c613c4a1a49d
tawhid12 <tawhid99@gmail.com>
2023-06-07 19:38:28 +0600

    correction

--- storage/framework/views/97432358720961ae2f37215d7046e9cadc3aa459.php
+++ storage/framework/views/97432358720961ae2f37215d7046e9cadc3aa459.php
@@ -40,100 +40,115 @@
     </section>
 
     <section class="page-section contact dark">
-        <div class="container">
+    <div class="container">
 
-            <!-- Get in touch -->
+        <!-- Get in touch -->
 
-            <h2 class="section-title">
-                <small><?php echo e(__('Feel Free to Say Hello!')); ?></small>
-                <span><?php echo e(__("Get in Touch With Us")); ?></span>
-            </h2>
+        <h2 class="section-title wow fadeInDown" data-wow-offset="200" data-wow-delay="100ms">
+            <small>Feel Free to Say Hello!</small>
+            <span><?php echo e(__("Get in Touch With Us")); ?></span>
+        </h2>
 
-            <div class="row">
-                <div class="col-md-6">
-                    <!-- Contact form -->
-                    <form name="contact-form" method="post" action="#" class="contact-form alt" id="contact-form">
+        <div class="row">
+            <div class="col-md-6 wow fadeInLeft" data-wow-offset="200" data-wow-delay="200ms">
+                <!-- Contact form -->
+                <form name="contact-form" method="post" action="#" class="contact-form" id="contact-form">
 
-                        <div class="row">
-                            <div class="col-md-6">
+                    <div class="row">
+                        <div class="col-md-6">
 
-                                <div class="outer required">
-                                    <div class="form-group af-inner has-icon">
-                                        <label class="sr-only" for="name"><?php echo e(__("Name")); ?></label>
-                                        <input type="text" name="name" id="name" placeholder="<?php echo e(__('Name')); ?>" value="" size="30" data-toggle="tooltip" title="" class="form-control placeholder" data-original-title="Name is required">
-                                        <span class="form-control-icon"><i class="fa fa-user"></i></span>
-                                    </div>
+                            <div class="outer required">
+                                <div class="form-group af-inner has-icon">
+                                    <label class="sr-only" for="name"><?php echo e(__("Name")); ?></label>
+                                    <input type="text" name="name" id="name" placeholder="Name" value="" size="30" data-toggle="tooltip" title="Name is required" class="form-control placeholder" />
+                                    <span class="form-control-icon"><i class="fa fa-user"></i></span>
                                 </div>
-
                             </div>
-                            <div class="col-md-6">
 
-                                <div class="outer required">
-                                    <div class="form-group af-inner has-icon">
-                                        <label class="sr-only" for="email"><?php echo e(__("Email")); ?></label>
-                                        <input type="text" name="email" id="email" placeholder="<?php echo e(__('Email')); ?>" value="" size="30" data-toggle="tooltip" title="" class="form-control placeholder" data-original-title="Email is required">
-                                        <span class="form-control-icon"><i class="fa fa-envelope"></i></span>
-                                    </div>
-                                </div>
-
-                            </div>
                         </div>
+                        <div class="col-md-6">
 
+                            <div class="outer required">
+                                <div class="form-group af-inner has-icon">
+                                    <label class="sr-only" for="email"><?php echo e(__("Email")); ?></label>
+                                    <input type="text" name="email" id="email" placeholder="Email" value="" size="30" data-toggle="tooltip" title="Email is required" class="form-control placeholder" />
+                                    <span class="form-control-icon"><i class="fa fa-envelope"></i></span>
+                                </div>
+                            </div>
+
+                        </div>
+                    </div>
+
+                    <div class="outer required">
                         <div class="form-group af-inner has-icon">
-                            <label class="sr-only" for="input-message"><?php echo e(__("Message")); ?></label>
-                            <textarea name="message" id="input-message" placeholder="<?php echo e(__('Message')); ?>" rows="4" cols="50" data-toggle="tooltip" title="" class="form-control placeholder" data-original-title="Message is required"></textarea>
+                            <label class="sr-only" for="subject"><?php echo e(__("Subject")); ?></label>
+                            <input type="text" name="subject" id="subject" placeholder="Subject" value="" size="30" data-toggle="tooltip" title="Subject is required" class="form-control placeholder" />
                             <span class="form-control-icon"><i class="fa fa-bars"></i></span>
                         </div>
+                    </div>
 
-                        <div class="outer required">
-                            <div class="form-group af-inner">
-                                <input type="submit"
-                                       name="submit" class="form-button form-button-submit btn btn-block btn-theme"
-                                       id="submit_btn" value="<?php echo e(__('Send message')); ?>">
-                            </div>
+                    <div class="form-group af-inner has-icon">
+                        <label class="sr-only" for="input-message"><?php echo e(__("Message")); ?></label>
+                        <textarea name="message" id="input-message" placeholder="Message" rows="4" cols="50" data-toggle="tooltip" title="Message is required" class="form-control placeholder"></textarea>
+                        <span class="form-control-icon"><i class="fa fa-bars"></i></span>
+                    </div>
+
+                    <div class="outer required">
+                        <div class="form-group af-inner">
+                            <input type="submit" name="submit" class="form-button form-button-submit btn btn-block btn-theme ripple-effect btn-theme-dark" id="submit_btn" value="Send message" />
                         </div>
+                    </div>
 
-                    </form>
-                    <!-- /Contact form -->
-                </div>
-                <div class="col-md-6">
-
-                    <p><?php echo e(__("This is Photoshop's version  of Lorem Ipsum. Proin gravida nibh vel velit auctor aliquet. Aenean sollicitudin, lorem quis bibendum auctor, nisi elit consequat ipsum.")); ?></p>
-
-                    <ul class="media-list contact-list">
-                        <li class="media">
-                            <div class="media-left"><i class="fa fa-home"></i></div>
-                            <div class="media-body"><?php echo e(__("Adress: 1600 Pennsylvania Ave NW, Washington, D.C.")); ?></div>
-                        </li>
-                        <li class="media">
-                            <div class="media-left"><i class="fa fa"></i></div>
-                            <div class="media-body"><?php echo e(__("DC 20500, ABD")); ?></div>
-                        </li>
-                        <li class="media">
-                            <div class="media-left"><i class="fa fa-phone"></i></div>
-                            <div class="media-body"><?php echo e(__("Support Phone: 01865 339665")); ?></div>
-                        </li>
-                        <li class="media">
-                            <div class="media-left"><i class="fa fa-envelope"></i></div>
-                            <div class="media-body"><?php echo e(__("E mails: info@example.com")); ?></div>
-                        </li>
-                        <li class="media">
-                            <div class="media-left"><i class="fa fa-clock-o"></i></div>
-                            <div class="media-body"><?php echo e(__("Working Hours: 09:30-21:00 except on Sundays")); ?></div>
-                        </li>
-                        <li class="media">
-                            <div class="media-left"><i class="fa fa-map-marker"></i></div>
-                            <div class="media-body"><?php echo e(__("View on The Map")); ?></div>
-                        </li>
-                    </ul>
-
-                </div>
+                </form>
+                <!-- /Contact form -->
             </div>
+            <div class="col-md-6 wow fadeInRight" data-wow-offset="200" data-wow-delay="200ms">
 
-            <!-- /Get in touch -->
+                <p><?php echo e(__(" At Perfect Line,Our customer's satisfaction comes first,We provide mobility solutions that benefit")); ?></p>
 
+                <ul class="media-list contact-list">
+                    <li class="media">
+                        <div class="media-left"><i class="fa fa-home"></i></div>
+                        <div class="media-body"><?php echo e(__("Adress: Dubai")); ?></div>
+                    </li>
+                    <li class="media">
+                        <div class="media-left"><i class="fa fa"></i></div>
+                        <div class="media-body"><?php echo e(__("")); ?></div>
+                    </li>
+                    <li class="media">
+                        <div class="media-left"><i class="fa fa-phone"></i></div>
+                        <div class="media-body"><?php echo e(__("Support Phone: +971 54 495 7070")); ?></div>
+                    </li>
+                    <li class="media">
+                        <div class="media-left"><i class="fa fa-envelope"></i></div>
+                        <div class="media-body"><?php echo e(__("E mails: perfectlinerac@yahoo.com")); ?></div>
+                    </li>
+
+                    <!--<li class="media">
+                        <div class="media-left"><i class="fa fa-clock-o"></i></div>
+                        <div class="media-body">
+                            <p><?php echo e(__("Working Hours: 9:00 AM - 10:00 PM on Monday")); ?></p>
+                            <p><?php echo e(__("Working Hours: 8:30 AM - 10:00 PM on Tuesday")); ?></p>
+                            <p><?php echo e(__("Working Hours: 9:00 AM - 10:00 PM on Wednesday")); ?></p>
+                            <p><?php echo e(__("Working Hours: 9:00 AM - 10:00 PM on Thursday")); ?></p>
+                            <p><?php echo e(__("Working Hours: 9:00 AM - 10:00 PM on Thursday")); ?></p>
+                            <p><?php echo e(__("Working Hours: 9:00 AM - 10:00 PM on Saturday")); ?></p>
+                            <p><?php echo e(__("Working Hours: 8:30 AM - 9:00 PM on Sunday")); ?></p>
+                        </div>
+                    </li>
+                    <li class="media">
+                        <div class="media-left"><i class="fa fa-map-marker"></i></div>
+                        <div class="media-body"><?php echo e(__("View on The Map")); ?></div>
+                    </li>-->
+                </ul>
+            </div>
         </div>
-    </section>
+
+        <!-- /Get in touch -->
+
+
+    </div>
+</section>
 <?php $__env->stopSection(); ?>
 
 

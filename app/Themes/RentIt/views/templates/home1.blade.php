53f0252a5389ce9636f80318e6f01b4b217d8512
tawhid12 <tawhid99@gmail.com>
2023-06-08 20:20:42 +0600

    correction

--- app/Themes/RentIt/views/templates/home1.blade.php
+++ app/Themes/RentIt/views/templates/home1.blade.php
@@ -687,52 +687,7 @@ alt="Testimonial avatar">
     </div>
 </section>--}}
 
-<script type="text/javascript">
-    var
-        mapObject,
-        markers = [],
-        markersData = {
-            !!$markersData!!
-        }
-    };
 
-
-    function initialize_map() {
-
-
-        loadScript("/rentit/js/infobox.js", after_load);
-
-    }
-
-    function after_load() {
-        var global_scrollwheel = false;
-        var markerClusterer = null;
-        var markerCLuster;
-        var Clusterer;
-
-        initialize_new2();
-    }
-
-    function loadScript(src, callback) {
-        var s,
-            r,
-            t;
-        r = false;
-        s = document.createElement('script');
-        s.type = 'text/javascript';
-        s.src = src;
-        s.onload = s.onreadystatechange = function() {
-            ////console.log( this.readyState ); //uncomment this line to see which ready states are called.
-            if (!r && (!this.readyState || this.readyState == 'complete')) {
-                r = true;
-                callback();
-            }
-        };
-        t = document.getElementsByTagName('script')[0];
-        t.parentNode.insertBefore(s, t);
-
-    }
-</script>
 <!-- /PAGE -->
 
 

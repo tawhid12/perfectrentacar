e80d1891e51ea6c790bb19104b677a29b78cec4d
tawhid12 <tawhid99@gmail.com>
2023-06-14 20:47:54 +0600

    correction

--- app/Themes/RentIt/views/templates/home1.blade.php
+++ app/Themes/RentIt/views/templates/home1.blade.php
@@ -664,6 +664,21 @@ alt="Testimonial avatar">
 </section>-->
 <!-- /PAGE -->
 
+<!-- PAGE -->
+<section class="page-section no-padding no-bottom-space-off">
+    <div class="container full-width">
+
+        <!-- Google map -->
+        <div class="google-map">
+            <div id="map-canvas">
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3037.4877706501666!2d-8.523064824745093!3d40.420196955429546!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xd23a9fa10153775%3A0x99491ccad21cba21!2sPerfeclima%2C%20Lda!5e0!3m2!1sen!2sbd!4v1686753438859!5m2!1sen!2sbd" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+            </div>
+        </div>
+        <!-- /Google map -->
+        
+
+    </div>
+</section>
 
 
 <!-- /PAGE -->

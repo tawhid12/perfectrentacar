859cd7f39ae2ee2f8c48c9ac9532eae89cfc03cf
tawhid12 <tawhid99@gmail.com>
2023-06-15 12:35:40 +0600

    correction

--- app/Themes/RentIt/views/layouts/header.blade.php
+++ app/Themes/RentIt/views/layouts/header.blade.php
@@ -146,7 +146,7 @@
                    
                
                     <ul class="social-icons">
-                        <li><a target="_blank" href="https://api.whatsapp.com/send?phone=971544957070" class="whatsapp"><i class="fa fa-whatsapp"></i></a>+971 54 495 7070</li>
+                        <li><a target="_blank" href="https://api.whatsapp.com/send?phone=971544957070" class="whatsapp"><i class="fa fa-whatsapp"></i></a><span>+971 54 495 7070</span></li>
                         <li><a target="_blank" href="https://www.facebook.com/PerfectLineRentaCar/" class="facebook"><i class="fa fa-facebook"></i></a></li>
                         <!-- <li><a target="_blank" href="" class="twitter"><i class="fa fa-twitter"></i></a></li> -->                        <!-- <li><a target="_blank" href="" class="linkedin"><i class="fa fa-linkedin"></i></a></li>
                         <li><a target="_blank" href="" class="linkedin"><i class="fa fa-google"></i></a></li> -->

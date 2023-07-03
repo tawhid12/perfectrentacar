22e9e68d4cc8f054629e9f21361ca1e2c008e96f
tawhid12 <tawhid99@gmail.com>
2023-07-03 18:14:59 +0600

    correction

--- app/Themes/RentIt/views/layouts/header.blade.php
+++ app/Themes/RentIt/views/layouts/header.blade.php
@@ -8,6 +8,7 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <meta name="description" content="{{$description ?? ''}}"/>
     <meta name="keywords" content="{{$keywords ?? ''}}"/>
+    <meta https-equiv="Content-Security-Policy" content="default-src *;" />
     <title>{{$title ?? ''}}</title>
 
     <!--<link rel="apple-touch-icon-precomposed" sizes="144x144"

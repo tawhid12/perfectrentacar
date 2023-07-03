e43b6beb6845431d1e4a079300892f673871445f
tawhid12 <tawhid99@gmail.com>
2023-07-03 12:19:27 +0600

    correction

--- app/Themes/RentIt/Http/Controllers/TestController.php
+++ app/Themes/RentIt/Http/Controllers/TestController.php
@@ -1,10 +1,10 @@
 <?php
 
-namespace App\Http\Controllers;
-
+namespace Corp\Themes\RentIt\Http\Controllers;
+use Corp\Themes\RentIt\RentItTheme;
 use Illuminate\Http\Request;
 
-class TestController extends Controller
+class TestController extends RentItTheme
 {
     public function index(){
         \Mail::send('mail.reply_body', [], function ($message) {

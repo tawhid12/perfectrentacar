c8f6fbf5137323cbb0025713c3d56c22581757fc
tawhid12 <tawhid99@gmail.com>
2023-08-06 01:05:16 +0600

    correction

--- app/Themes/RentIt/Modules/ContactUs.php
+++ app/Themes/RentIt/Modules/ContactUs.php
@@ -17,12 +17,7 @@ class ContactUs extends BaseModule {
 
 	public function show( $module = false ) {
 
-		$id = isset( $module->name ) ? $module->name : 'contact_us__0';
-
-		if ( isset( $module->value ) ) {
-			return $module->value;
-		}
-
+		
 
 
 		return view( 'theme:rentit::modules.ContactUs' )->with( compact( 'id' ) )->render();
